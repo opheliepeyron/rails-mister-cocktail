c13dc3d3ca5c8ad156d6ecc16c9cf5ca83dee2dc
Ophélie Peyron <ophelie.peyron@gmail.com>
2020-11-20 15:20:32 +0100

    add cocktails images

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -20,7 +20,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :ingredient_id)
+    params.require(:cocktail).permit(:name, :ingredient_id, :photo)
   end
 
 end
